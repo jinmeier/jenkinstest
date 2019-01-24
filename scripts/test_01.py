fd43ea65505f1b66e5153931e2850a0b82bfdb96
zhangsan <zhangsan@qq.com>
2019-01-24 16:43:59 +0800

    aaa

---
+++ scripts/test_01.py
@@ -0,0 +1,15 @@
+import allure
+
+def test_a():
+    print('aaa')
+    assert 1
+
+@allure.step(title="测试登录的流程")
+def test_login():
+    allure.attach('登录','输入用户名')
+    print('bbb')
+    allure.attach('登录', '输入密码')
+    print('ccc')
+    allure.attach('登录', '点击登录')
+    print('ddd')
+    assert 0
